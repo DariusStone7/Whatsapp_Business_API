82d13e4ff13c2f7aac83bb6bb1cbbb0653ea3222
Darius NGUETSA <nguetsadarius@yahoo.com>
2026-01-12 00:33:55 +0100

    test webhook subscription

--- src/main/java/com/whatsapp/whatsapp_business_api/controller/WhatsappMessage.java
+++ src/main/java/com/whatsapp/whatsapp_business_api/controller/WhatsappMessage.java
@@ -1,22 +1,30 @@
 package com.whatsapp.whatsapp_business_api.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.function.EntityResponse;
 
 import java.util.Map;
 
 @RestController
 public class WhatsappMessage {
 
+    @Value("${VERIFY_TOKEN}")
+    private String verify_token;
+
 @GetMapping("/")
     public ResponseEntity<String> verifyWebhook(@RequestParam(value = "hub.mode", required = false) String mode,
                                                 @RequestParam(value = "hub.challenge", required = false) String challenge,
                                                 @RequestParam(value = "hub.verify_token", required = false) String token){
-        System.out.println("mode: " + mode + " challenge: " + challenge + " token: " + token);
+    System.out.println("verify_token = " + this.verify_token);
+    System.out.println("mode: " + mode + " challenge: " + challenge + " token: " + token);
+    if(mode.equals("subscribe") && this.verify_token.equals(token)){
+        System.out.println("WEBHOOK VERIFY");
+        return ResponseEntity.ok(challenge);
+    }
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Error");
 
-        return ResponseEntity.ok("connected");
     }
 
     @PostMapping("/")
