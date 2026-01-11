f989c16c0cfc4984da65803645c8eb9b13e0e5f5
Darius NGUETSA <nguetsadarius@yahoo.com>
2026-01-12 00:11:58 +0100

    test webhook subscription

--- src/main/java/com/whatsapp/whatsapp_business_api/controller/WhatsappMessage.java
+++ src/main/java/com/whatsapp/whatsapp_business_api/controller/WhatsappMessage.java
@@ -5,18 +5,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.function.EntityResponse;
 
+import java.util.Map;
+
 @RestController
 public class WhatsappMessage {
 
 @GetMapping("/")
-    public ResponseEntity<String> verifyWebhook(@RequestParam() Object param){
-        System.out.println(param);
+    public ResponseEntity<String> verifyWebhook(@RequestParam(value = "hub.mode", required = false) String mode,
+                                                @RequestParam(value = "hub.challenge", required = false) String challenge,
+                                                @RequestParam(value = "hub.verify_token", required = false) String token){
+        System.out.println("mode: " + mode + " challenge: " + challenge + " token: " + token);
 
         return ResponseEntity.ok("connected");
     }
 
     @PostMapping("/")
-    public ResponseEntity<String> handleWebhook(@RequestBody() Object body){
+    public ResponseEntity<String> handleWebhook(@RequestBody() Map<String, Object> body){
         System.out.println(body);
 
         return  ResponseEntity.ok("Webhook recieved");
