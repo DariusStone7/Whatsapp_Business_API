46621e9850265610142186c0ef1e1322f6a8a0d3
Darius NGUETSA <nguetsadarius@yahoo.com>
2026-01-12 01:02:58 +0100

    correct .env usage

--- src/main/java/com/whatsapp/whatsapp_business_api/controller/WhatsappMessage.java
+++ src/main/java/com/whatsapp/whatsapp_business_api/controller/WhatsappMessage.java
@@ -1,6 +1,8 @@
 package com.whatsapp.whatsapp_business_api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,20 +12,27 @@ import java.util.Map;
 @RestController
 public class WhatsappMessage {
 
-    @Value("${VERIFY_TOKEN}")
+    private final Environment env;
     private String verify_token;
+
+    @Autowired
+    public WhatsappMessage(Environment env) {
+        this.env = env;
+        this.verify_token = env.getProperty("VERIFY_TOKEN");
+    }
 
 @GetMapping("/")
     public ResponseEntity<String> verifyWebhook(@RequestParam(value = "hub.mode", required = false) String mode,
                                                 @RequestParam(value = "hub.challenge", required = false) String challenge,
                                                 @RequestParam(value = "hub.verify_token", required = false) String token){
-    System.out.println("verify_token = " + this.verify_token);
-    System.out.println("mode: " + mode + " challenge: " + challenge + " token: " + token);
-    if(mode.equals("subscribe") && this.verify_token.equals(token)){
-        System.out.println("WEBHOOK VERIFY");
-        return ResponseEntity.ok(challenge);
-    }
-    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Error");
+
+        System.out.println("verify_token = " + this.verify_token);
+        System.out.println("mode: " + mode + " challenge: " + challenge + " token: " + token);
+        if(mode.equals("subscribe") && this.verify_token.equals(token)){
+            System.out.println("WEBHOOK VERIFY");
+            return ResponseEntity.ok(challenge);
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Error");
 
     }
 
